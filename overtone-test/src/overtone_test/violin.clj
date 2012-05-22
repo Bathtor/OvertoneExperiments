762088aa800c35453bb52220a1e1dcfc15d92aa3
Bathtor <bathtor@googlemail.com>
2012-05-22 21:03:23 +0200

    some changes to overtone-tes

--- overtone-test/src/overtone_test/violin.clj
+++ overtone-test/src/overtone_test/violin.clj
@@ -2,12 +2,76 @@
   (:use [overtone.core]))
 (connect-external-server 57110)
 (use 'overtone.inst.synth)
+(use 'overtone.sc.ugens)
+(use 'overtone.sc.envelope)
+(use 'overtone.sc.defcgen)
+(volume 50)
 
-(demo 5
-      (comb-l
-       (rlpf (* 0.05 (lf-pulse (mul-add (f-sin-osc:kr 0.05 0) 80 160) 0 0.4))
-             (mul-add (f-sin-osc:kr [0.6 0.7] 0) 3600 4000)
-             0.2)
-       0.3 [0.2 0.25] 2))
+(definst dampedwave
+  [in 440 dampening 0.5]
+  (let [sig (sin-osc in)
+        delay (reciprocal in)
+        delayed-sig (delay-n sig delay delay)]
+    (resonz (+ sig delayed-sig) 440 dampening)))
+
+(defcgen guitar-string
+  "Single guitar string"
+  [note {:default 60 :doc "note played on that string"}
+   velocity {:default 80 :doc "midi type velocity"}]
+  "Models a single guitar string using physical modeling."
+  (let [freq (midicps note)
+        vol (/ velocity 127)
+        stroke (env-gen (adsr 0.02 3.005 1.0 vol) 1 :action FREE)
+        length (/ (reciprocal freq) 2)
+        [fb1 fb2] (local-in 2)
+        up-delay (delay-l fb1 0.05 length)
+        down-delay (delay-l fb2 0.05 length)
+        disp-filt (apf down-delay freq)
+        loss-filt (one-pole (* disp-filt 0.99) 0.55)
+        up-refl (neg up-delay)
+        down-refl (neg loss-filt)
+        string-vel (+ down-refl up-refl)]
+    (local-out (+ stroke [down-refl up-refl]))
+    (resonz down-refl 440 0.5)))
+
+(definst guitar
+  [note 60 velocity 80]
+   (let [freq (midicps note)
+        vol (/ velocity 127)
+        stroke (env-gen (adsr 0.02 3.005 1.0 vol) 1 :action FREE)
+        length (/ (reciprocal freq) 2)
+        [fb1 fb2] (local-in 2)
+        up-delay (delay-l fb1 0.05 length)
+        down-delay (delay-l fb2 0.05 length)
+        disp-filt (apf down-delay freq)
+        loss-filt (one-pole (* disp-filt 0.99) 0.55)
+        up-refl (neg up-delay)
+        down-refl (neg loss-filt)
+        string-vel (+ down-refl up-refl)]
+    (local-out (+ stroke [down-refl up-refl]))
+    (resonz down-refl 440 0.5)))
+
+(guitar 63)
+
+(stop)
+
+(dampedwave 220 0.01)
+
+(definst violin
+  [note 60]
+  (let [freq (midicps note)
+        sig (sin-osc freq)
+        delay (/ (reciprocal freq) 1)
+        [fb1 fb2] (local-in 2)
+        neck-delay (delay-l (+ fb1 sig) 0.05 delay)
+        bridge-delay (delay-l (+ fb2 sig) 0.05 delay)
+        neck-refl (neg neck-delay)
+        bridge-refl (neg bridge-delay)]
+    (local-out (* [bridge-refl neck-refl] 1))
+    (+ bridge-delay neck-delay)))
+
+(violin 69)
+
+(bowed 69)
 
 (stop)
